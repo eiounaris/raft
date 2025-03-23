eda8864d8423be68efff9c1e01fe3cd2b984b7b2
eiounaris <eiounaris@gmail.com>
2025-03-23 10:18:13 +0800

    add kvdb_test.go

---
+++ go-raft-server/kvdb/kvdb_test.go
@@ -0,0 +1,47 @@
+package kvdb
+
+import "testing"
+
+func TestKVDB(t *testing.T) {
+
+	db, err := MakeKVDB("tmp/db")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if value, err := db.Get([]byte("myname")); value != nil || err == nil {
+		t.Error()
+	}
+
+	if err := db.Set([]byte("myname"), []byte("zhangsan")); err != nil {
+		t.Error(err)
+	}
+
+	if value, err := db.Get([]byte("myname")); string(value) != "zhangsan" || err != nil {
+		t.Error()
+	}
+
+	if err := db.Delete([]byte("myname")); err != nil {
+		t.Error(err)
+	}
+
+	if value, err := db.Get([]byte("myname")); value != nil || err == nil {
+		t.Error()
+	}
+
+	if err := db.Set([]byte("myname"), []byte{}); err != nil {
+		t.Error(err)
+	}
+
+	if value, err := db.Get([]byte("myname")); string(value) != "" || err != nil {
+		t.Error()
+	}
+
+	if err := db.Delete([]byte("myname")); err != nil {
+		t.Error(err)
+	}
+
+	if err := db.Close(); err != nil {
+		t.Error(err)
+	}
+}
